a26890aec2419d486deb974a306c2e46ee898439
vonhutuanhoiio <vonhutuan@hoiio.com>
2014-06-09 11:29:41 +0700

    Update server.rb
    Create directory for logstash

--- recipes/server.rb
+++ recipes/server.rb
@@ -7,6 +7,12 @@
 # License: MIT
 #
 
+# Create directory for logstash
+directory "/etc/logstash/conf.d" do
+  action :create
+  mode '0755'
+end
+
 template "/etc/logstash/conf.d/server.conf" do
   source "logstash.conf.erb"
   variables( :config => node[:logstash][:server] )
